20f914770ef3bc25e39f8d99392a74669ac0ecd9
Yahya Unlu <yhunlu87@gmail.com>
2021-06-29 00:36:48 +0300

    improving welcome screen with AppButton

--- App.js
+++ App.js
@@ -7,15 +7,5 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AppButton title="Login" onPress={() => console.log("Tapped")} />
-    </View>
-  );
+  return <WelcomeScreen />;
 }

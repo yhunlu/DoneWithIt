b4e1a166173ae47b0fa48ec2c8dc3a36da9126ee
Yahya Unlu <yhunlu87@gmail.com>
2021-06-29 01:17:21 +0300

    added card-component

--- App.js
+++ App.js
@@ -6,6 +6,24 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
+
+import Card from "./app/components/Card";
+
+import colors from "./app/config/colors";
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: colors.white,
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red Jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }

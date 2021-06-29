894b417a9922f72ca4f9cc78d263fa54ca6336dd
Yahya Unlu <yhunlu87@gmail.com>
2021-06-30 00:40:43 +0300

    added Listitem components

--- app/components/ListingDetailsScreen.js
+++ app/components/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 
 import AppText from "./AppText";
+import ListItem from "./ListItem";
 
 import colors from "../config/colors";
 function ListingDetailsScreen(props) {
@@ -11,6 +12,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.subTitle}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/me.jpg")}
+            title="Yahya Unlu"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -33,6 +41,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 30,
   },
 });
 

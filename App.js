592987bca90ecf1e14c26d4a0b105520b0a39b4a
Yahya Unlu <yhunlu87@gmail.com>
2021-07-05 23:25:34 +0300

    added AppTextInputJS component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -11,6 +11,7 @@ import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
 import ListItem from "./app/components/ListItem";
+import AppTextInput from "./app/components/AppTextInput";
 
 import MessagesScreen from "./app/screens/MessagesScreen";
 import AccountScreen from "./app/screens/AccountScreen";
@@ -18,5 +19,10 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 
 import colors from "./app/config/colors";
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }

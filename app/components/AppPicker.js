6b7e84a1850c895177e8a872e910806829bda4e2
Yahya Unlu <yhunlu87@gmail.com>
2021-07-13 23:36:45 +0300

    added ListingEditScreenJS

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -30,9 +30,11 @@ function AppPicker({ icon, items, placeholder, onSelectItem, selectedItem }) {
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>
-            {selectedItem ? selectedItem.label : placeholder}
-          </AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -75,6 +77,10 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   text: {
+    flex: 1,
+  },
+  placeholder: {
+    color: defaultStyles.colors.medium,
     flex: 1,
   },
 });

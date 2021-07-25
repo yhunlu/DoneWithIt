48d740f2e9f9d722a4479579f97ae0cd434c160b
yhunlu <yhunlu87@gmail.com>
2021-07-26 01:27:45 +0300

    added NewListingButton for tabs

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -6,7 +6,7 @@ import ListingDetailsScreen from "../screens/ListingDetailsScreen";
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
-  <Stack.Navigator mode="modal">
+  <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Listings" component={ListingsScreen} />
     <Stack.Screen
       name="ListingDetails"
---
+++ app/navigation/NewListingButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+function NewListingButton({ onPress }) {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="plus-circle"
+          color={colors.white}
+          size={40}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    borderColor: colors.white,
+    borderRadius: 40,
+    borderWidth: 10,
+    bottom: 30,
+    height: 80,
+    justifyContent: "center",
+    width: 80,
+  },
+});
+
+export default NewListingButton;

eae543eb6ec51406adaf68d83bce21c0431a0b3f
YAHYA UNLU <yhunlu87@gmail.com>
2025-06-04 13:19:21 +0300

    Install dependencies for testing

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -1,20 +1,30 @@
 import { useState } from "react";
+import cache from "../utility/cache";
 
-export default useApi = (apiFunc) => {
+export default function useApi(apiFunc, cacheKey) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const request = async () => {
+  const request = async (...args) => {
+    if (cacheKey) {
+      const cachedData = await cache.get(cacheKey);
+      if (cachedData) setData(cachedData);
+    }
     setLoading(true);
-    const response = await apiFunc();
+    const response = await apiFunc(...args);
     setLoading(false);
 
-    if (!response.ok) return setError(true);
+    if (!response.ok) {
+      setError(true);
+      return response;
+    }
     setError(false);
 
     setData(response.data);
+    if (cacheKey) cache.store(cacheKey, response.data);
+    return response;
   };
 
   return { data, error, loading, request };
-};
+}
